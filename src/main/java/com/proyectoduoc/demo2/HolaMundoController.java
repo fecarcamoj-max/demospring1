cee2de4bb600c79dd1278ec3f6666c0bc96013b5
ingfelipeacj <felipecarcamo@upla.cl>
2026-06-27 09:24:33 -0400

    ADD cambio en mensaje 2

--- src/main/java/com/proyectoduoc/demo2/HolaMundoController.java
+++ src/main/java/com/proyectoduoc/demo2/HolaMundoController.java
@@ -13,7 +13,7 @@ public class HolaMundoController {
 
     @GetMapping("/")
     public String getMethodName() {
-        return "Hola Mundo 09:19:00 27 06 2026 !! Cierre de act práctica pipeline";
+        return "Hola Mundo 09:24:00 27 06 2026 !! Cierre de act práctica pipeline";
     }
 
     @GetMapping("/getCar")
